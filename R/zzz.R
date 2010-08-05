a1506258392ec329d9b3efe5bbf13f0b4691d362
Sophie Ancelet <Sophie.Ancelet@orange.fr>
2010-08-05 00:00:00 +0000

    version 1.0.1

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 {
   library.dynam("Geneclust",pkg, lib)
   cat('**********************************************************',fill=TRUE)
-  cat('*   Geneclust 1.0.0 is loaded                            *',fill=TRUE)
+  cat('*   Geneclust 1.0.1 is loaded                            *',fill=TRUE)
   cat('*                                                        *',fill=TRUE)
   cat('*   Use help(Geneclust) for a quick overview.            *',fill=TRUE)
   cat('*                                                        *',fill=TRUE)
